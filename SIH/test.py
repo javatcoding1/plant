bfa6d0b3f3dc6069d13210689806c5d510e46920
jayanth <100996407+javatcoding1@users.noreply.github.com>
2023-09-19 22:44:43 +0530

    Add files via upload

---
+++ SIH/test.py
@@ -0,0 +1,14 @@
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# Define a route for the 'flexbox2.html' page
+@app.route('/')
+def flexbox2():
+    return render_template('welcomepage.html')
+@app.route('/flexbox2')
+def flexbox1():
+    return render_template('flexbox2.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
